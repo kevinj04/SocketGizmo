8aa55667b6e125f4b8ce972e6bd95eed3959dbca
Kevin Jenkins <kevinj04@gmail.com>
2012-03-23 13:13:22 -0400

    Initial Commit

---
+++ SocketGizmo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SocketGizmo
+//
+//  Created by Kevin Jenkins on 3/23/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
