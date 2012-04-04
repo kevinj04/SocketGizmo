51f4f77cb98a6db8b3268df449d1f34aa3c1a495
Kevin Jenkins <kevinj04@gmail.com>
2012-04-04 14:37:39 -0400

    Added a new target for static library.

---
+++ SocketManager/SocketManager.h
@@ -0,0 +1,13 @@
+//
+//  SocketManager.h
+//  SocketManager
+//
+//  Created by Kevin Jenkins on 4/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SocketManager : NSObject
+
+@end
---
+++ SocketManager/SocketManager.m
@@ -0,0 +1,13 @@
+//
+//  SocketManager.m
+//  SocketManager
+//
+//  Created by Kevin Jenkins on 4/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SocketManager.h"
+
+@implementation SocketManager
+
+@end
